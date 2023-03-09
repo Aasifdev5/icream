f3ead2fe9d69df78439c82a45b2c811ab1912034
Aasif Ahmed <asifarslaan@gmail.com>
2023-03-09 15:38:45 +0530

    as on now

--- routes/web.php
+++ routes/web.php
@@ -17,24 +17,24 @@ use App\Http\Controllers\ProductController;
 // Route::get('/', function () {
 //     return view('index');
 // });
-// Route::get('/index', function () {
-//     return view('index');
-// });
-// Route::get('/about',function(){
-//     return view('about');
-// });
+Route::get('/index', function () {
+    return view('index');
+});
+Route::get('/about',function(){
+    return view('about');
+});
 // Route::get('/product',function(){
 //     return view('product');
 // });
-// Route::get('/service',function(){
-//     return view('service');
-// });
-// Route::get('/gallery',function(){
-//     return view('gallery');
-// });
-// Route::get('/contact',function(){
-//     return view('contact');
-// });
+Route::get('/service',function(){
+    return view('service');
+});
+Route::get('/gallery',function(){
+    return view('gallery');
+});
+Route::get('/contact',function(){
+    return view('contact');
+});
 
 Route::get('/',[ProductController::class,'index'])->name('home');
 Route::get('/product',[ProductController::class,'product'])->name('product');
